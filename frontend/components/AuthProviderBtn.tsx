0f3d4d38885ecbbe25ad641d178c00bc391d4743
leoroullois <roullois.leo@gmail.com>
2022-03-11 00:35:43 +0100

    refactor <next> : login & register

---
+++ frontend/components/AuthProviderBtn.tsx
@@ -0,0 +1,29 @@
+import { FC, MouseEventHandler } from "react";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import classnames from "classnames";
+import scss from "../scss/login.module.scss";
+
+interface IProps {
+	provider: string;
+	svg: string;
+}
+const AuthProviderBtn: FC<IProps> = ({ provider, svg }) => {
+	const router = useRouter();
+
+	const handleClick: MouseEventHandler = (e) => {
+		e.preventDefault();
+		router.push(`/auth/${provider.toLowerCase()}`);
+	};
+	return (
+		<button
+			onClick={handleClick}
+			className={classnames(scss["provider"], provider.toLowerCase())}
+		>
+			<Image className={scss["provider--img"]} src={svg} alt={provider + " logo"} height={35} width={35} />
+			<p>Connexion avec {provider}</p>
+		</button>
+	);
+};
+
+export default AuthProviderBtn;
---
+++ frontend/pages/se-connecter.tsx
@@ -0,0 +1,111 @@
+import { FormEventHandler, useState, useEffect } from "react";
+import Link from "next/link";
+import { useDispatch, useSelector } from "react-redux";
+import { login } from "../store/slices/auth";
+import google from "/public/google.svg";
+import github from "/public/github.svg";
+import scss from "/scss/login.module.scss";
+import AuthProviderBtn from "../components/AuthProviderBtn";
+import { selectAuth } from "../store/selectors";
+import { useRouter } from "next/router";
+import Head from "next/head";
+
+export const fetchApiCall = (url: string, method: string, body = {}) =>
+	fetch(url, {
+		method,
+		body: JSON.stringify(body),
+	});
+
+const Login: React.FC<any> = () => {
+	const auth = useSelector(selectAuth);
+	const router = useRouter();
+	const dispatch = useDispatch();
+
+	const [state, setState]: [ILoginState, any] = useState({
+		email: "",
+		password: "",
+		remember: false,
+	});
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setState({ ...state, [e.target.name]: e.target.value });
+	};
+	const handleSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+		e.preventDefault();
+		login(state);
+	};
+	useEffect(() => {
+		if (auth.isAuthenticated) {
+			router.push("/dashboard");
+		}
+	}, [auth.isAuthenticated, router]);
+
+	return (
+		<>
+			<Head>
+				<title>Se connecter - PrépaStat</title>
+			</Head>
+			<main className={scss["login"]}>
+				<h1>Se connecter</h1>
+				<h2>Content de vous revoir !</h2>
+				<form
+					action='/se-connecter'
+					method='post'
+					className={scss["login-form"]}
+					onSubmit={handleSubmit}
+				>
+					<label htmlFor='login-email' id='login-email-label'>
+						Email
+					</label>
+					<input
+						type='email'
+						name='email'
+						id='login-email'
+						placeholder='Entrez votre email'
+						required
+						value={state.email}
+						onChange={handleChange}
+					/>
+					<label htmlFor='login-password' id='login-password-label'>
+						Mot de passe
+					</label>
+					<input
+						type='password'
+						name='password'
+						id='login-password'
+						placeholder='Entrez votre mot de passe'
+						required
+						value={state.password}
+						onChange={handleChange}
+					/>
+					<div className={scss["login-remember-forgot-container"]}>
+						<div className={scss["login-remember-container"]}>
+							<input
+								type='checkbox'
+								name='remember'
+								id='login-remember'
+								onChange={handleChange}
+							/>
+							<label htmlFor='login-remember'>Souvenez-vous de moi.</label>
+						</div>
+						<Link href='/mot-de-passe-oublie'>
+							<a className={scss["login-forgot"]}>Mot de passe oublié</a>
+						</Link>
+					</div>
+					<button type='submit' className={scss["login-submit"]}>
+						Connexion
+					</button>
+					<AuthProviderBtn svg={google} provider='Google' />
+					<AuthProviderBtn svg={github} provider='Github' />
+				</form>
+				<div className={scss["no-account"]}>
+					<p>Pas de compte ?</p>
+					<Link href='/s-enregistrer'>
+						<a>S&apos;enregistrer</a>
+					</Link>
+				</div>
+			</main>
+		</>
+	);
+};
+
+export default Login;
